7f51b7cf75d6338617b321f564c02a4ca28aec44
dong <ddliuhb@gmail.com>
2014-06-20 00:53:00 +0800

    co

--- motto.go
+++ motto.go
@@ -2,23 +2,83 @@ package motto
 
 import (
     "github.com/robertkrimen/otto"
+    "strings"
 )
 
 type Motto interface {
     otto.Otto
     modules map[string]*Module
+    currentModule *Module
 }
 
-func (otto *Otto) RunModule(moduleFile string) (Value, error) {
-    
+func (motto *Motto) RunModule(moduleFile string) (otto.Value, error) {
+        
+}
+
+func (motto *Motto) Inject() {
+    motto.Set("require", func(call otto.FunctionCall) otto.Value {
+        filename, err := call.Argument(0).ToString()
+        if err != nil || filename == "" || !strings.HasSuffix(strings.ToLower(filename), ".js") {
+            return otto.UndefinedValue()
+        }
+
+        currentModule = motto.currentModule
+
+        if !filepath.IsAbs(filename) {
+            filename, err = filepath.Abs(filepath.Join(filepath.Dir(currentModule.Filename), filename))
+            if err != nil {
+                return otto.UndefinedValue()
+            }
+
+            existModule, ok := motto.modules[filename]
+            // exist
+            if ok {
+                // Circle reference
+                if !existModule.Loaded {
+                    panic(call.Otto.Call("new Error", nil, "Circle module reference detected"))
+                } else {
+                    return existModule.Value
+                }
+            }
+
+        }
+
+        f, err := ioutil.ReadFile(filename)
+        if err == nil {
+            panic(call.Otto.Call("new Error", nil, fmt.Sprintf("Module file cannot be read: %s", filename)))
+        }
+
+        v, err := call.Otto.Run(f)
+
+        module := &Module {
+            Id: filename,
+            Filename: filename,
+            Loaded: false,
+        }
+
+        motto.modules[filename] = module
+
+        motto.currentModule = module
+
+
+        call.Otto.run()
+
+
+        currentModule.Filename
+
+
+
+
+    })
 }
 
 type Module struct {
     Id string
     Filename string
     Loaded bool
-    Parent *Module
-    Children []*Module
+    // Parent *Module
+    // Children []*Module
+    Value otto.Value
 }
 
 func NewModule(moduleFile string) *Module {
@@ -34,4 +94,3 @@ func Run(moduleFile string) (*Motto, otto.Value, error) {
 
     return otto, value, err
 }
-
